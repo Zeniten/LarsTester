4bd2c714f1a893f5957da5cf4bb951ba588dfa1e
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-07-30 00:20:56 +0200

    Add anagram search field

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class SearchFrame extends Component {
     this.state = { search: "" };
   }
 
-  handleOnUpdate = (e) => {
+  handleSearch = (e) => {
     this.setState({ search: e.target.value });
   }
   
-  handleClick = (e) => {
+  handleSearchClick = (e) => {
     e.preventDefault(); // Preventing reloading of page.
 
     this.props.onHandleClick(this.state.search);
@@ -23,8 +23,8 @@ class SearchFrame extends Component {
     return (
       <div className="SearchWord">
         <form>
-          <input type="text" value={this.state.search} onChange={this.handleOnUpdate}/>
-          <button type="submit" onClick={this.handleClick}>
+          <input type="text" value={this.state.search} onChange={this.handleSearch}/>
+          <button type="submit" onClick={this.handleSearchClick}>
             Søk
           </button>
         </form>
@@ -33,7 +33,7 @@ class SearchFrame extends Component {
   }
 }
 
-class ResultFrame extends Component {
+class SearchResultFrame extends Component {
   render() {
     var result, test = this.props.test;
     if (this.props.searchWord !== "") {
@@ -63,18 +63,63 @@ class ResultFrame extends Component {
   }
 }
 
+class AnagramsFrame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { anagram: "" }
+  }
+
+  handleAnagram = (e) => {
+    this.setState({ anagram: e.target.value });
+  }
+
+  handleAnagramClick = (e) => {
+    e.preventDefault();
+    
+    this.props.serveAnagrams(this.state.anagram);
+  }
+
+  render() {
+    return (
+        <form>
+          <h2>Finn anagrammer</h2>
+          <input type="text" value={this.state.anagram} onChange={this.handleAnagram}/>
+          <button type="submit" onClick={this.handleAnagramClick}>
+            Søk
+          </button>
+        </form>  
+    )
+  }
+}
+
+class AnagramsResultFrame extends Component {
+  render() {
+    return (
+      <div>
+      <p>Denne strengen har {this.props.specAnagrams.length} anagrammer.</p>
+      <ul>
+        {this.props.specAnagrams.map(function(word) {
+          return <p>{word}</p>;
+        })}
+      </ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       wordlist: data.words,
       searchWord: "",
+      anagrams: {},
+      specificAnagrams: ["lev", "vel", "elv"],
       test: false
     };
   }
 
   handleClick = (word) => {
-
     var w = word.toUpperCase();
     this.setState({ searchWord: w,
                     test: false });
@@ -84,6 +129,39 @@ class App extends Component {
         this.setState({ test: true });
       }
     }
+  }
+
+  serveAnagrams = (word) => {
+    var str = word.split("").sort().join("").toUpperCase();
+    var anagrams = this.state.anagrams;
+
+    if (str in anagrams) {
+      this.setState({ specificAnagrams: anagrams[str] });
+    } else {
+      this.setState({ specificAnagrams: ["laaars"] });
+    }
+  }
+
+  findAnagrams = () => {
+    var str = "", wordlist = this.state.wordlist;
+    var agrms = {};
+
+    for (var i = 0; i < wordlist.length; i++) {
+      str = wordlist[i].split("").sort().join("");
+
+      if (str in agrms) {
+        if (!agrms[str].includes(wordlist[i])) {
+          agrms[str].push(wordlist[i]);
+        }
+      } else {
+        agrms[str] = [wordlist[i]];
+      }
+    }
+    this.setState({ anagrams: agrms });
+  }
+
+  componentWillMount() {
+    this.findAnagrams();
   }
 
   render() {
@@ -96,7 +174,9 @@ class App extends Component {
           <h1>Søk i ordlisten</h1>
           <SearchFrame wordlist={this.state.wordlist}
                       onHandleClick={this.handleClick} />
-          <ResultFrame test={this.state.test} searchWord={this.state.searchWord} />
+          <SearchResultFrame test={this.state.test} searchWord={this.state.searchWord} />
+          <AnagramsFrame serveAnagrams={this.serveAnagrams} />
+          <AnagramsResultFrame specAnagrams={this.state.specificAnagrams} />
       </div>
     );
   }
